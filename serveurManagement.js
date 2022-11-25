7df0c9086c4a2f405ca3c17d12f8c8cab5132e72
safouanezouine <zouinesafouane@gmail.com>
2022-11-25 16:01:42 +0100

    ajout des routes principaux

--- serveurManagement.js
+++ serveurManagement.js
@@ -2,6 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+app.get('/', function(req, res) {
+    res.send('hello acceuil');
+});
+
+app.get('/train/', function(req, res) {
+  res.send('hello train');
+});
+
+app.get('/station/', function(req, res) {
+  res.send('hello station');
+});
+
+
+app.get('/voyage/', function(req, res) {
+  res.send('hello voyage');
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+  console.log(`Example app listening at http://localhost:${port}`)
+});
