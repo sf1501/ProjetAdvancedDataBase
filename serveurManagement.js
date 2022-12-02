cfa4b0e1d525a4b8b3dc4301d707439dab827f16
safouanezouine <zouinesafouane@gmail.com>
2022-12-02 16:05:45 +0100

    mise a jour

--- serveurManagement.js
+++ serveurManagement.js
@@ -49,7 +49,7 @@ app.get('/voyage/:id', function(req, res) {
   requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE id_voyage=${voyageId}`
   res.send(requette);
 });
-app.get('/voyage/:type', function(req, res) {
+app.get('/voyage/type/:type', function(req, res) {
   parametre  = res.params
   typeVoyage = parametre.type
   requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE type=${typeVoyage}`
@@ -57,7 +57,7 @@ app.get('/voyage/:type', function(req, res) {
 });
 
 // requette pour recuperer tous les voyage qui part de la gare_depart souhaité
-app.get('/voyage/:gare_depart', function(req, res) {
+app.get('/voyage/gare_depart/:gare_depart', function(req, res) {
   parametre  = res.params
   gareD = parametre.gare_depart
   requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE gare_depart=${gareD}`
@@ -65,7 +65,7 @@ app.get('/voyage/:gare_depart', function(req, res) {
 });
 // requette pour recuperer tous les voyage qui arrive  a la gare_arrivee souhaité
 
-app.get('/voyage/:gare_arrivee', function(req, res) {
+app.get('/voyage/gare_arrivee/:gare_arrivee', function(req, res) {
   parametre  = res.params
   gareA = parametre.gare_arrivee
   requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE gare_arrivee=${gareA}`
@@ -73,7 +73,7 @@ app.get('/voyage/:gare_arrivee', function(req, res) {
 });
 // requette pour recuperer tous les voyage qui part a heure_depart souhaité
 
-app.get('/voyage/:heure_depart', function(req, res) {
+app.get('/voyage/heure_depart/:heure_depart', function(req, res) {
   parametre  = res.params
   heureD = parametre.heure_depart
   requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE heure_depart=${heureD}`
@@ -82,7 +82,7 @@ app.get('/voyage/:heure_depart', function(req, res) {
 
 // requette pour recuperer tous les voyage qui arrive a l'heure_arrivee souhaité
 
-app.get('/voyage/:heure_arrivee', function(req, res) {
+app.get('/voyage/heure_arrivee/:heure_arrivee', function(req, res) {
   parametre  = res.params
   heureA = parametre.heure_arrivee
   requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE heure_arrivee=${heureA}`
@@ -90,7 +90,7 @@ app.get('/voyage/:heure_arrivee', function(req, res) {
 });
 // requette pour recuperer tous les voyage qui arrive avec retard souhaité
 
-app.get('/voyage/:retard', function(req, res) {
+app.get('/voyage/retard/:retard', function(req, res) {
   parametre  = res.params
   retarD = parametre.retard
   requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE retard=${retarD}`
