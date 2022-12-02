c5f1bb620cbb5cdb05372a0cca5aa9b377b7229d
safouanezouine <zouinesafouane@gmail.com>
2022-12-02 15:20:31 +0100

    ajout des routes principaux

--- serveurManagement.js
+++ serveurManagement.js
@@ -9,17 +9,61 @@ app.get('/', function(req, res) {
 });
 
 app.get('/train/', function(req, res) {
-  res.send('hello train');
+  idTrain = req.params
+
+  res.send('hello station '+idTrain);
 });
 
-app.get('/station/', function(req, res) {
-  res.send('hello station');
+app.get('/train/:idTrain', function(req, res) {
+  trainId = req.params.idTrain
+  requette = `SELECT id_train name capacite id_voyage FROM train.db WHERE id_train=${trainId}`
+
+  res.send('hello station '+idTrain);
 });
 
 
-app.get('/voyage/', function(req, res) {
-  res.send('hello voyage');
+
+
+app.get('/station/:idStation', function(req, res) {
+  stationId = req.params.idStation
+  requette = `SELECT id_gare name nb_voie FROM gard.db WHERE id_gare=${stationId}`
+  res.send(requette);
 });
+
+app.get('/station/',function(req,res){
+
+})
+
+
+app.get('/voyage/:id', function(req, res) {
+  parametre  = res.params
+  voyageId = parametre.id
+  requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE id_voyage=${voyageId}`
+  res.send(requette);
+});
+app.get('/voyage/:type', function(req, res) {
+  parametre  = res.params
+  typeVoyage = parametre.type
+  requette = `SELECT id_voyage type gare_depart gare_arrivee heure_depart heure_arrivee retard FROM voyage.db WHERE type=${typeVoyage}`
+  res.send(requette);
+});
+
+app.get('/voyage/:gare_depart', function(req, res) {
+  res.send(requette);
+});
+app.get('/voyage/:gare_arrivee', function(req, res) {
+  res.send(requette);
+});
+app.get('/voyage/:heure_depart', function(req, res) {
+  res.send(requette);
+});
+app.get('/voyage/:heure_arrivee', function(req, res) {
+  res.send(requette);
+});
+app.get('/voyage/:retard', function(req, res) {
+  res.send(requette);
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
